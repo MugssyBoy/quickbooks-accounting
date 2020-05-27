35534b2a93887172a6760f6384f79ab38eff4e19
alex <43148847+MugssyBoy@users.noreply.github.com>
2020-05-28 02:59:49 +0800

    added accounting functionalities

--- src/qb.js
+++ src/qb.js
@@ -1,3 +1,4 @@
+const key = require('ckey');
 const OAuthClient = require('intuit-oauth');
 
 let oauthClient, authUri;
@@ -5,30 +6,78 @@ let oauthClient, authUri;
 class Quickbooks {
 
     createConnection(config) {
-        oauthClient = new OAuthClient(config);
-        authUri = oauthClient.authorizeUri({
-            scope: [
-                OAuthClient.scopes.Accounting
-            ]
-        })
-        return authUri
+        try {
+            oauthClient = new OAuthClient(config);
+            authUri = oauthClient.authorizeUri({ scope: [OAuthClient.scopes.Accounting] });
+            return authUri;
+        } catch (err) {
+            return err;
+        }
     }
 
-    createToken(url) {
-        let uri = oauthClient.createToken(url)
-        console.log(uri)
+    async getToken(url) {
+        try {
+            let token = await oauthClient.createToken(url);
+            return token.json;
+        } catch (err) {
+            return err;
+        }
     }
 
+    async getVendor() {
+        try {
+            const companyID = oauthClient.getToken().realmId;
+            const url = oauthClient.environment == "sandbox" ? OAuthClient.environment.sandbox : OAuthClient.environment.production;
+            let vendor = await oauthClient.makeApiCall({ url: `${url}v3/company/${companyID}/query?query=select%20*%20from%20vendor&minorversion=51` });
+            return vendor;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    async getCompanyInfo() {
+        try {
+            const companyID = oauthClient.getToken().realmId;
+            const url = oauthClient.environment == "sandbox" ? OAuthClient.environment.sandbox : OAuthClient.environment.production;
+            let companyName = await oauthClient.makeApiCall({ url: `${url}v3/company/${companyID}/companyinfo/${companyID}` });
+            return companyName;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    async getClass() {
+        try {
+            const companyID = oauthClient.getToken().realmId;
+            const url = oauthClient.environment == "sandbox" ? OAuthClient.environment.sandbox : OAuthClient.environment.production;
+            let companyName = await oauthClient.makeApiCall({ url: `${url}v3/company/${companyID}/companyinfo/${companyID}` });
+            return companyName;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    async getAccount(params) {
+        try {
+            const companyID = oauthClient.getToken().realmId;
+            const url = oauthClient.environment == "sandbox" ? OAuthClient.environment.sandbox : OAuthClient.environment.production;
+            let account = await oauthClient.makeApiCall({ url: `${url}v3/company/${companyID}/query?query=select%20*%20from%20Account%20where%20Classification%20=%20'${params}'&minorversion=51` });
+            return account;
+        } catch (err) {
+            return err
+        }
+    }
+
+    async getTerm() {
+        try {
+            const companyID = oauthClient.getToken().realmId;
+            const url = oauthClient.environment == "sandbox" ? OAuthClient.environment.sandbox : OAuthClient.environment.production;
+            let term = await oauthClient.makeApiCall({ url: `${url}v3/company/${companyID}/query?query=select%20*%20from%20Term` });
+            return term;
+        } catch (err) {
+            return err
+        }
+    }
 };
 
 module.exports = Quickbooks;
-
-// let qb = new Quickbooks()
-// let uri = qb.createConnection({
-//     clientId: "ABF9gSNPxeO3SNThCWyTMEVFk4Di1DmWFYo6UOxOAgVFhQ7W05",
-//     clientSecret: "Oc8sWrJd6Hl6y5TBz8Rx54bORRnI8MSrJBcem8S5",
-//     environment: "sandbox",
-//     redirectUri: "http://localhost:8000/callback"
-//     //redirectUri: `https://${req.hostname}/callback`
-// })
-// console.log(uri)
